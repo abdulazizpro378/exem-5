7f86311d445ff10adae134ba1ec773cf0b5f1b55
abdulazizpro378 <abdujazizabdi045@gmail.com>
2023-08-17 18:20:35 +0500

    salom

--- src/pages/user/PostP.jsx
+++ src/pages/user/PostP.jsx
@@ -1,6 +1,7 @@
 import { Image } from "antd";
 import { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { IMG_URL } from "../../const";
 // import { IMG_URL } from "../../const";
 import { request } from "../../server/request";
 
@@ -29,13 +30,27 @@ const PostP = () => {
           <div className="div-row">
             <div className="img-post">
               {/* {console.log(post.photo._id)} */}
-              <Image className="img4" style={{height:'600px', width:'100%'}} src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
+              {/* <Image className="img4" style={{height:'600px', width:'100%'}} src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" /> */}
               {/* <Image
                   src={
                     IMG_URL + post.photo._id + "." + post.photo.name.split(".")[1]
                   }
                   alt="Photo"
                 /> */}
+                  {post.photo ? (
+                  <Image
+                    src={`${IMG_URL}${post.photo._id}.${
+                      post.photo.name.split(".")[1]
+                    }`}
+                    onError={(e) => {
+                      e.target.onerror = null;
+                      e.target.src = "https://picsum.photos/1000/1000";
+                    }}
+                    style={{height:'600px', width:'100%'}}
+                  />
+                ) : (
+                  <div>eror image </div>
+                )}
             </div>
             <h1>{post.title} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, molestiae!</h1>
             {/* <p>Startup (#business, #screen, #life):  {post.updatedAt.split("T")[0]}</p> */}
